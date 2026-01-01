54f1397550eac961cab3a3ca7ad8adac18d97d1f
horiz <horiz@Horizon.com>
2026-01-01 13:03:10 -0500

    Basic fading orbital trail.

--- src/06_orbital_trails/main.cpp
+++ src/06_orbital_trails/main.cpp
@@ -1,41 +1,50 @@
-#include "raylib.h"
+#include <raylib.h>
+#include <deque>
 #include <cmath>
-#include <iostream>
 
-using namespace std;
-
-int main(void)
-{
+int main() {
     const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "Orbital Pixel Trail");
 
-    InitWindow(screenWidth, screenHeight, "Orbit Trail");
-
-    SetTargetFPS(60);
-
-    const Vector2 center = { screenWidth/2.0f, screenHeight/2.0f };
+    // Orbit parameters
+    const Vector2 center = {(float)screenWidth / 2, (float)screenHeight / 2};
     const float radius = 150.0f;
-    const float speed = 1.0f;
+    const float speed = 2.0f;               // radians per second
+    const size_t maxTrailLength = 200;      // number of pixels to keep
+
+    std::deque<Vector2> trail;
     float angle = 0.0f;
 
-    while (!WindowShouldClose())
-    {
-        angle += speed * GetFrameTime();
-        if (angle > 2 * PI) angle -= 2 * PI;
-        Vector2 pos = {
-            center.x + radius * cos(angle),
-            center.y + radius * sin(angle)
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        // ---- Update -------------------------------------------------
+        float dt = GetFrameTime();
+        angle += speed * dt;                 // advance angle
+        if (angle > 2 * PI) angle -= 2 * PI; // keep within [0,2π)
+
+        // Compute new pixel position
+        Vector2 newPos = {
+            center.x + radius * std::cos(angle),
+            center.y + radius * std::sin(angle)
         };
+        trail.push_back(newPos);
+        if (trail.size() > maxTrailLength) trail.pop_front();
+
+        // ---- Draw ---------------------------------------------------
         BeginDrawing();
-            ClearBackground(BLACK);
-            DrawCircleV(center, 5, RED);
-            DrawCircleLinesV(center, radius, GREEN);
-            DrawCircleV(pos, 20, MAROON);
-        
+        ClearBackground(BLACK);
+
+        // Draw trail with fading opacity
+        for (size_t i = 0; i < trail.size(); ++i) {
+            float t = (float)i / trail.size();          // 0 = oldest, 1 = newest
+            Color col = Fade(WHITE, t);                 // older pixels more transparent
+            DrawPixelV(trail[i], col);
+        }
+
         EndDrawing();
     }
 
     CloseWindow();
-
     return 0;
 }
