f75e9e8026c8e3d32955ded3178582298a341cce
horiz <horiz@Horizon.com>
2026-02-17 21:34:13 -0500

    Update the mouse follow code.

--- src/04_circle_collision/main.cpp
+++ src/04_circle_collision/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 #include <math.h>
 
 int main(void) {
@@ -11,17 +12,48 @@ int main(void) {
     Vector2 sclerRightPosition = { GetScreenWidth()/2.0f + 100.0f, GetScreenHeight()/2.0f };
     float sclerRadius = 80;
 
-    Vector2 irisLeftPosition = { GetScreenWidth()/2.0f - 100.0f, GetScreenHeight()/2.0f };
-    Vector2 irisRightPosition = { GetScreenWidth()/2.0f + 100.0f, GetScreenHeight()/2.0f };
+    Vector2 irisLeftPosition = sclerLeftPosition;
+    Vector2 irisRightPosition = sclerRightPosition;
     float irisRadius = 24;
-
-    float angle = 0.0f;
-    float dx = 0.0f, dy = 0.0f, dxx = 0.0f, dyy = 0.0f;
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        iris
+        Vector2 mouse = GetMousePosition();
+
+        // left eye
+        Vector2 dirLeft = Vector2Subtract(mouse, sclerLeftPosition);
+        float lenLeft = Vector2Length(dirLeft);
+
+        if (lenLeft > (sclerRadius - irisRadius)) {
+            dirLeft = Vector2Scale(Vector2Normalize(dirLeft), sclerRadius - irisRadius);
+        }
+
+        irisLeftPosition = Vector2Add(sclerLeftPosition, dirLeft);
+
+        // right eye
+        Vector2 dirRight = Vector2Subtract(mouse, sclerRightPosition);
+        float lenRight = Vector2Length(dirRight);
+
+        if (lenRight > (sclerRadius - irisRadius)) {
+            dirRight = Vector2Scale(Vector2Normalize(dirRight), sclerRadius - irisRadius);
+        }
+
+        irisRightPosition = Vector2Add(sclerRightPosition, dirRight);
+
+        // drawing
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        // sclera - white part
+        DrawCircleV(sclerLeftPosition, sclerRadius, LIGHTGRAY);
+        DrawCircleV(sclerRightPosition, sclerRadius, LIGHTGRAY);
+
+        // iris
+        DrawCircleV(irisLeftPosition, irisRadius, BLUE);
+        DrawCircleV(irisRightPosition, irisRadius, BLUE);
+
+        EndDrawing();
     }
 
     CloseWindow();
