bd6464d7376379750a96119623b2a2587acc8b74
horiz <horiz@Horizon.com>
2026-02-21 20:35:32 -0500

    Upadte with 3D plane and canera movment.

--- src/12_basic_ik_robot/main.cpp
+++ src/12_basic_ik_robot/main.cpp
@@ -7,12 +7,34 @@ int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
-    InitWindow(screenWidth, screenHeight, "raylib");
+
+    InitWindow(screenWidth, screenHeight, "IK Robot Simulator");
+
+    Camera camera = {
+        { 8.0f, 4.0f, 8.0f },
+        { 0.0f, 1.0f, 0.0f },
+        { 0.0f, 1.0f, 0.0f },
+        35.0f,
+        CAMERA_PERSPECTIVE
+    };
+
     SetTargetFPS(60);
+
     while(!WindowShouldClose()) {
+
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+            UpdateCamera(&camera, CAMERA_FREE);
+        }
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hi", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(BLACK);
+
+        BeginMode3D(camera);
+
+            DrawGrid(20, 1.0f);
+        
+        EndMode3D();
+
         EndDrawing();
     }
     CloseWindow();
