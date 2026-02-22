a04ce45e38d87ebf159d256c5f47320daac71c5a
horiz <horiz@Horizon.com>
2026-02-22 18:21:42 -0500

    Better robot look.

--- src/12_basic_ik_robot/main.cpp
+++ src/12_basic_ik_robot/main.cpp
@@ -34,7 +34,7 @@ int main()
     arm[0].pos = {xOffset, 0.1f, zOffset};
     // First joint
     arm[1].pos = {xOffset - 1.0f, 1.5f, zOffset};
-    // Second joint
+    // Second joint / end-effector
     arm[2].pos = {xOffset + 1.0f, 2.7f, zOffset};
 
     Vector3 basePos = {xOffset, 0.0f, zOffset};
@@ -66,18 +66,28 @@ int main()
             DrawCubeV({xOffset + 0.5f, 0.25f, zOffset}, {0.5f, 0.05f, 0.05f}, GREEN);
 
             // --- Servo caps ---
-            DrawCylinder(basePos, 0.2f, 0.2f, 0.05f, 16, DARKGRAY);
-            DrawCylinder(arm[0].pos, 0.2f, 0.2f, 0.05f, 16, DARKGRAY);
-            DrawCylinder(arm[1].pos, 0.2f, 0.2f, 0.05f, 16, DARKGRAY);
+            DrawCylinder(basePos, 0.2f, 0.2f, 0.05f, 32, DARKGRAY);
+            DrawCylinder(arm[0].pos, 0.2f, 0.2f, 0.05f, 32, DARKGRAY);
+            DrawCylinder(arm[1].pos, 0.2f, 0.2f, 0.05f, 32, DARKGRAY);
+
+            // --- Arm segments with metallic shading ---
+            for (size_t i = 0; i < arm.size() - 1; i++)
+            {
+                float brightness = 0.6f + 0.4f * (arm[i].pos.y / 3.0f);
+                Color segmentColor = { (unsigned char)(70 * brightness),
+                                       (unsigned char)(130 * brightness),
+                                       (unsigned char)(180 * brightness), 255 };
+                DrawCylinderEx(arm[i].pos, arm[i+1].pos, 0.1f, 0.08f, 32, segmentColor);
+            }
 
             // --- Joints ---
-            DrawSphere(arm[0].pos, 0.15f, BLUE);
-            DrawSphere(arm[1].pos, 0.15f, BLUE);
-            DrawSphere(arm[2].pos, 0.15f, RED);
-
-            // --- Arm segments ---
             for (size_t i = 0; i < arm.size() - 1; i++)
-                DrawCylinderEx(arm[i].pos, arm[i+1].pos, 0.1f, 0.08f, 16, BLUE);
+            {
+                DrawSphere(arm[i].pos, 0.16f, BLUE);
+            }
+            // End-effector with glow
+            DrawSphere(arm[2].pos, 0.18f, RED);
+            DrawSphereWires(arm[2].pos, 0.2f, 16, 16, ORANGE);
 
         EndMode3D();
 
