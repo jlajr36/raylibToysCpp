cb7b49b085ed7afb9a83793652bc5f8ebd2bdd43
horiz <horiz@Horizon.com>
2026-04-18 09:24:59 -0400

    Start Flying Terrain project.

---
+++ src/20_Flying_Terrain/main.cpp
@@ -0,0 +1,18 @@
+#include "raylib.h"
+#include <iostream>
+
+int main()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "raylib");
+    SetTargetFPS(60);
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hi", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
