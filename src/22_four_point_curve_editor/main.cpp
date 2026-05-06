de5a877572af449abdf82eef80a702256abf6ac9
horiz <horiz@Horizon.com>
2026-05-06 17:08:16 -0400

    Add mouse position.

--- src/22_four_point_curve_editor/main.cpp
+++ src/22_four_point_curve_editor/main.cpp
@@ -4,14 +4,29 @@ int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
+
     SetConfigFlags(FLAG_WINDOW_HIGHDPI | FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "4-Point Curve Editor");
     SetTargetFPS(60);
-    while(!WindowShouldClose()) {
+
+    Vector2 mousePos = {0, 0};
+
+    while (!WindowShouldClose())
+    {
+        mousePos = GetMousePosition();
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        DrawText(
+            TextFormat("Mouse: (%.0f, %.0f)", mousePos.x, mousePos.y),
+            10, 10, 20,
+            DARKGRAY
+        );
+
         EndDrawing();
     }
+
     CloseWindow();
     return 0;
 }
