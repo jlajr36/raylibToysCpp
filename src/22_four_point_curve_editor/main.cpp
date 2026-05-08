2cde80c81bc782e82d12e1a72cfa7e4eb7dfafa5
horiz <horiz@Horizon.com>
2026-05-08 06:59:58 -0400

    Remove custom Bezier function.

--- src/22_four_point_curve_editor/main.cpp
+++ src/22_four_point_curve_editor/main.cpp
@@ -1,15 +1,5 @@
 #include "raylib.h"
 
-Vector2 CubicBezier(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float t) {
-    float u = 1.0f - t;
-    float tt = t*t, uu = u*u, uuu = uu*u, ttt = tt*t;
-
-    Vector2 p;
-    p.x = uuu*p0.x + 3*uu*t*p1.x + 3*u*tt*p2.x + ttt*p3.x;
-    p.y = uuu*p0.y + 3*uu*t*p1.y + 3*u*tt*p2.y + ttt*p3.y;
-    return p;
-}
-
 bool IsMouseNearPoint(Vector2 p, float radius) {
     return CheckCollisionPointCircle(GetMousePosition(), p, radius);
 }
